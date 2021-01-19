78c777165c488bb46864b46a96529833ab3d3401
elimerl <elimerlindavies@gmail.com>
2021-01-18 21:17:16 -0800

    Done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,81 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap';
+(document.getElementById('start') as HTMLButtonElement).onclick = onClickStart;
+function onClickStart() {
+  if (!worker) worker = new Worker('/dist/worker.js', { type: 'module' });
+  worker.onmessage = (ev) => {
+    const data: (Swimmer | null)[][] = ev.data.data;
+    render(data);
+  };
+  start();
+  (document.getElementById('start') as HTMLButtonElement).innerText = 'Stop';
+  (document.getElementById('start') as HTMLButtonElement).onclick = onClickStop;
+}
+function onClickStop() {
+  worker.terminate();
+  //@ts-expect-error
+  worker = undefined;
+  (document.getElementById('start') as HTMLButtonElement).innerText = 'Start';
 
-import confetti from 'canvas-confetti';
+  (document.getElementById(
+    'start',
+  ) as HTMLButtonElement).onclick = onClickStart;
+}
+let worker = new Worker('/dist/worker.js', { type: 'module' });
+function start() {
+  worker.postMessage({
+    msg: 'start',
+    parameters: {
+      nfish: parseInt(
+        (document.getElementById('nfish') as HTMLInputElement).value,
+      ),
+      nshark: parseInt(
+        (document.getElementById('nshark') as HTMLInputElement).value,
+      ),
+      fbreed: parseInt(
+        (document.getElementById('fbreed') as HTMLInputElement).value,
+      ),
+      sbreed: parseInt(
+        (document.getElementById('sbreed') as HTMLInputElement).value,
+      ),
+      starve: parseInt(
+        (document.getElementById('starve') as HTMLInputElement).value,
+      ),
+    },
+  });
+}
+document.getElementById;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+ctx.scale(4, 4);
+function render(data: (Swimmer | null)[][]) {
+  console.log('rendering');
+  //@ts-expect-error
+  const fishArr: Fish[] = data.flat(1).filter((v) => v?.isFish);
+  //@ts-expect-error
+  const sharks: Shark[] = data.flat(1).filter((v) => v?.isShark);
+  //@ts-expect-error
+  document.getElementById('counts').innerText = `${fishArr.length} fish
+  ${sharks.length} sharks`;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  data.forEach((row, y) => {
+    row.forEach((val, x) => {
+      if (val?.isFish) {
+        ctx.fillStyle = 'blue';
+      } else if (val?.isShark) {
+        ctx.fillStyle = 'green';
+      } else {
+        ctx.fillStyle = 'black';
+      }
+      ctx.fillRect(x, y, 1, 1);
+    });
+  });
+}
+interface Swimmer {
+  age: number;
+  x: number;
+  y: number;
+  isFish?: true;
+  isShark?: true;
+}
---
+++ src/worker.ts
@@ -0,0 +1,185 @@
+/// <reference lib="webworker"/>
+const ctx: Worker = self as any;
+function mod(a: number, n: number) {
+  return ((a % n) + n) % n;
+}
+const sample = (arr: number[][]) => arr[Math.floor(Math.random() * arr.length)];
+class Simulation {
+  parameters: IWatorParameters;
+  data: (Swimmer | null)[][];
+  width: number;
+  height: number;
+  constructor(parameters: IWatorParameters, width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.parameters = parameters;
+    this.data = Array.from({ length: height }, () => Array(width).fill(null));
+    while (
+      this.data.flat().filter((v) => v?.isFish).length < parameters.nfish
+    ) {
+      const pos = [
+        Math.floor(Math.random() * this.width),
+        Math.floor(Math.random() * this.height),
+      ];
+      const fish = new Fish(pos[0], pos[1]);
+      fish.age = Math.floor(Math.random() * parameters.fbreed);
+      this.set(pos[0], pos[1], fish);
+    }
+    while (
+      this.data.flat().filter((v) => v?.isShark).length < parameters.nshark
+    ) {
+      const pos = [
+        Math.floor(Math.random() * this.width),
+        Math.floor(Math.random() * this.height),
+      ];
+      const shark = new Shark(pos[0], pos[1]);
+      shark.breedAge = Math.floor(Math.random() * parameters.sbreed);
+      this.set(pos[0], pos[1], shark);
+    }
+  }
+  tick() {
+    console.log('Running tick');
+    // seperate out fish and sharks
+    //@ts-expect-error
+    const fishArr: Fish[] = this.data.flat(1).filter((v) => v?.isFish);
+    //@ts-expect-error
+    const sharks: Shark[] = this.data.flat(1).filter((v) => v?.isShark);
+
+    // fish swim and breed
+    fishArr.forEach((fish) => {
+      // age
+      fish.age++;
+      // move randomly
+      const possiblePositions = this.neighbors(fish.x, fish.y).filter(
+        (pos) => this.get(pos[0], pos[1]) === null,
+      );
+      if (possiblePositions.length < 1) {
+        return;
+      }
+      const newPos = sample(possiblePositions);
+      const oldPos = [fish.x, fish.y];
+      fish.x = newPos[0];
+      fish.y = newPos[1];
+      this.move(oldPos[0], oldPos[1], newPos[0], newPos[1]);
+      // if time, breed
+      if (fish.age >= this.parameters.fbreed) {
+        // create new fish and place it at shark's last position
+        const baby = new Fish(oldPos[0], oldPos[1]);
+        this.set(baby.x, baby.y, baby);
+      }
+    });
+    // shark hunt and breed
+    sharks.forEach((shark) => {
+      let oldPos: number[];
+      //age
+      shark.lastEaten++;
+      shark.breedAge++;
+
+      // starve
+      if (shark.lastEaten >= this.parameters.starve) {
+        this.set(shark.x, shark.y, null);
+        return;
+      }
+      const fishPositions = this.neighbors(shark.x, shark.y).filter(
+        (pos) => this.get(pos[0], pos[1])?.isFish,
+      );
+      if (fishPositions.length < 1) {
+        // there are no fish, so move randomly
+        const possiblePositions = this.neighbors(shark.x, shark.y).filter(
+          (pos) => this.get(pos[0], pos[1]) === null,
+        );
+        if (possiblePositions.length < 1) {
+          return;
+        }
+        const newPos = sample(possiblePositions);
+        oldPos = [shark.x, shark.y];
+        shark.x = newPos[0];
+        shark.y = newPos[1];
+        this.move(oldPos[0], oldPos[1], newPos[0], newPos[1]);
+      } else {
+        // there are fish!
+        const newPos = sample(fishPositions);
+        oldPos = [shark.x, shark.y];
+        this.set(newPos[0], newPos[1], null);
+        shark.x = newPos[0];
+        shark.y = newPos[1];
+        this.move(oldPos[0], oldPos[1], newPos[0], newPos[1]);
+        shark.lastEaten = 0;
+      }
+      if (shark.breedAge === this.parameters.sbreed) {
+        // create new fish and place it at shark's last position
+        const baby = new Shark(oldPos[0], oldPos[1]);
+        this.set(baby.x, baby.y, baby);
+      }
+    });
+  }
+  neighbors(x: number, y: number) {
+    return [
+      this.inBounds(x + 1, y),
+      this.inBounds(x - 1, y),
+      this.inBounds(x, y + 1),
+      this.inBounds(x, y - 1),
+    ];
+  }
+  set(x: number, y: number, val: Swimmer | null) {
+    this.data[mod(y, this.height)][mod(x, this.width)] = val;
+  }
+  get(x: number, y: number) {
+    return this.data[mod(y, this.height)][mod(x, this.width)];
+  }
+  inBounds(x: number, y: number) {
+    return [mod(y, this.height), mod(x, this.height)];
+  }
+  move(x: number, y: number, newX: number, newY: number) {
+    const item = this.get(x, y);
+    this.set(x, y, null);
+    this.set(newX, newY, item);
+  }
+}
+class Swimmer {
+  age: number;
+  x: number;
+  y: number;
+  isFish?: true;
+  isShark?: true;
+  constructor(x: number, y: number) {
+    this.age = 0;
+    this.x = x;
+    this.y = y;
+  }
+}
+class Fish extends Swimmer {
+  constructor(x: number, y: number) {
+    super(x, y);
+    this.isFish = true;
+  }
+}
+class Shark extends Swimmer {
+  lastEaten: number;
+  breedAge: number;
+  constructor(x: number, y: number) {
+    super(x, y);
+    this.isShark = true;
+    this.lastEaten = 0;
+    this.breedAge = 0;
+  }
+}
+
+let sim: Simulation;
+onmessage = (ev) => {
+  sim = new Simulation(ev.data.parameters, 200, 200);
+  setTimeout(handle, 20);
+};
+function handle() {
+  sim.tick();
+  postMessage(sim);
+  setTimeout(handle, 20);
+}
+interface IWatorParameters {
+  nfish: number;
+  nshark: number;
+  fbreed: number;
+  sbreed: number;
+  starve: number;
+}
+export {};
